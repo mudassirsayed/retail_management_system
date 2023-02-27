c52b79fd1d27427fe3c5d7e220f0674bf9623f93
@mudassir2 <mudassir@nimapinfotech.com>
2023-02-27 18:09:23 +0530

    created two pages and fetched products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import products from "./db.json";
+import Home from "./pages/Home";
+import Sales from "./pages/Sales";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchProducts = () => {
+    setData(products);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sales" element={<Sales />} />
+      </Routes>
+    </Router>
   );
 }
 
